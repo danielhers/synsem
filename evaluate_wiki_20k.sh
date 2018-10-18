0ed8a2d10c67482948990f118b93e91ffd5dd506
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-10-18 10:45:24 +0300

    Echo evaluated files

--- evaluate_wiki_20k.sh
+++ evaluate_wiki_20k.sh
@@ -14,9 +14,10 @@ for d in ${CORPORA[@]}; do
   for MODEL in ${UCCA_UD_MODEL[$d]} ${UCCA_MODEL[$d]} ${UDPIPE_MODEL[$d]} ${UD_MODEL[$d]}; do
       GUESSED=$PARSED/xml/$MODEL/$d
       REF=${DATA[$d]}
+      echo $GUESSED $REF
       python -m semstr.evaluate $GUESSED $REF --unlabeled -s $OUT/ucca/$MODEL.unlabeled.csv -q $*
       python -m semstr.evaluate $GUESSED $REF --constructions=categories -s $OUT/ucca/$MODEL.csv -o $OUT/all/ucca/$MODEL.csv -q $*
-      python -m semstr.evaluate $GUESSED $REF --constructions=categories -s $OUT/ud/$MODEL.csv -o $OUT/all/ud/$MODEL.csv -q --ref-yield-tags=$PARSED/conllu/${UDPIPE_MODEL[$d]}/$d.conllu $*
+      python -m semstr.evaluate $GUESSED $REF --constructions=categories -s $OUT/ud/$MODEL.csv -o $OUT/all/ud/$MODEL.csv --ref-yield-tags=$PARSED/conllu/${UDPIPE_MODEL[$d]}/$d.conllu -q $*
   done
 done
 
