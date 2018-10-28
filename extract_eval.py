f9a6341cfdddf89deeaae3a4aec87c005e0868e4
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-10-28 16:58:10 +0200

    Extract counts of reference categories matched to evaluated ones

--- extract_eval.py
+++ extract_eval.py
@@ -23,25 +23,39 @@ COLUMNS = pd.Series(list(map("_".join, chain(
     product(["UCCA", "CoNLL-U"], ["primary", "remote"], ["unlabeled", "labeled"], ["precision", "recall", "f1"]),
     product(["UCCA", "CoNLL-U"], UCCA_RELS + UD_RELS, ["labeled"], ["f1"])))))
 
+COUNTS_COLUMNS = pd.Series(list(map("_".join, product(
+    ["UCCA", "CoNLL-U"], UCCA_RELS + UD_RELS, ["labeled"], ["ref"]))))
+
+Corpus = namedtuple("Corpus", ("index", "name"))
 
 CORPORA = {
-    "ewt":        (0, "English EWT"),
-    "wiki-dev":   (1, "English Wiki dev"),
-    "20k":        (2, "English 20K"),
-    "20k-fr-dev": (3, "French 20K"),
-    "20k-de-dev": (4, "German 20K"),
+    "ewt":        Corpus(0, "English EWT"),
+    "wiki-dev":   Corpus(1, "English Wiki dev"),
+    "20k":        Corpus(2, "English 20K"),
+    "20k-fr-dev": Corpus(3, "French 20K"),
+    "20k-de-dev": Corpus(4, "German 20K"),
 }
 
 
-class Data(namedtuple("Data", ("columns", "report", "scores"))):
+class Data(namedtuple("Data", ("columns", "count_columns", "report", "scores", "counts"))):
     def column_names(self):
         return [c for c, e in zip(COLUMNS, self.columns) if e]
 
-    def ref(self):
-        return self.column_names()[0].partition("_")[0]  # e.g. UCCA_primary_labeled_precision -> UCCA
+    def counts_column_names(self):
+        return [c for c, e in zip(COUNTS_COLUMNS, self.count_columns) if e]
+
+    def ref(self):  # UCCA/CoNLL-U, indicating the format of the reference
+        try:
+            column_name = (self.column_names() or self.counts_column_names())[0]
+        except IndexError as e:
+            raise ValueError("No relevant columns found in '%s'" % self.report.filename) from e
+        try:
+            return column_name.partition("_")[0]  # e.g. UCCA_primary_labeled_precision -> UCCA
+        except IndexError as e:
+            raise ValueError("Failed to parse column name '%s'" % column_name) from e
 
 
-class Report(namedtuple("Report", ("is_ref", "model", "features", "ref", "labeled", "corpus", "filename"))):
+class Report(namedtuple("Report", ("is_ref", "model", "features", "ref", "labeled", "counts", "corpus", "filename"))):
     @staticmethod
     def create(f):
         basename = os.path.basename(re.match(r"(.*?)\.[a-z.]+", f).group(1))
@@ -49,7 +63,7 @@ class Report(namedtuple("Report", ("is_ref", "model", "features", "ref", "labele
         path = f.split(os.sep)
         # noinspection PyTypeChecker
         fields = (bool(m),) + (m.groups() if m else (basename, "")) + \
-                 (path[-2], "unlabeled" not in f, CORPORA[path[-3]], f)
+                 (path[-2], "unlabeled" not in f, "counts" in f, CORPORA[path[-3]], f)
         return Report(*fields)
 
     def read(self):
@@ -58,23 +72,33 @@ class Report(namedtuple("Report", ("is_ref", "model", "features", "ref", "labele
         except pd.errors.EmptyDataError as e:
             raise IOError("Failed reading '%s'" % self.filename) from e
         cs = COLUMNS.isin(list(df.columns))
-        return Data(list(cs), self, (df[list(COLUMNS[cs])] * 100).round(1))
+        ccs = COUNTS_COLUMNS.isin(list(df.columns))
+        return Data(columns=list(cs), count_columns=list(ccs), report=self,
+                    scores=(df[list(COLUMNS[cs])] * 100).round(1), counts=df[list(COUNTS_COLUMNS[ccs])])
 
 
 def eval_corpus(corpus, reports):
     key = attrgetter("model", "features")
     data = sorted(r.read() for _, rs in groupby(sorted(reports), key=key) for r in rs)
     print()
-    print(*([corpus[1]] + 14 * [""] + strip(sorted(set(c for d in data for c in d.column_names()[6:])))), sep="\t")
+    columns = list(map(strip, sorted(set(c for d in data for c in d.column_names()[6:]))))
+    print(*([corpus.name] + 14 * [""] + columns), sep="\t")
     for tup in groupby(data, key=Data.ref):
-        eval_ref(*tup)
+        eval_ref(*tup, columns=columns)
 
 
-def eval_ref(ref, data):
+def eval_ref(ref, data, columns=None):
     print()
-    print(ref)
     key = attrgetter("report.model", "report.features")
-    for tup in groupby(sorted(data, key=key), key=key):
+    data = sorted(data, key=key)
+    count_columns = [c for c in COUNTS_COLUMNS if ref in c]
+    count_columns = dict(zip(map(strip, count_columns), count_columns))
+    counts = combine(data, "counts").astype(int)
+    counts["zero"] = 0
+    ccs = [count_columns[c] for c in columns]
+    ccs = [c if c in counts.columns.tolist() else "zero" for c in ccs]
+    print(*([ref] + 14 * [""] + [counts[ccs].to_csv(header=False, index=False, sep="\t")]), sep="\t", end="")
+    for tup in groupby(data, key=key):
         eval_model_features(*tup)
 
 
@@ -82,9 +106,7 @@ def eval_model_features(model_features, data):
     model, features = model_features
     model = model.replace("v1-sentences", "ucca")
     data = list(data)
-    df = data[0].scores
-    for data in data[1:]:
-        df = df.combine_first(data.scores)
+    df = combine(data)
     if not df.eq(100).all().all():
         columns = df.columns.tolist()
         cs = [c for c in COLUMNS if c in columns]
@@ -97,13 +119,22 @@ def eval_model_features(model_features, data):
               df[cs].to_csv(header=False, index=False, sep="\t").strip().replace("\n", "\n\t\t\t"), sep="\t")
 
 
-def strip(cs):
-    return [c.replace("UCCA_", "").replace("CoNLL-U_", "").replace("_labeled_f1", "") for c in cs]
+def combine(data, attr="scores"):
+    df = getattr(data[0], attr)
+    for data in data[1:]:
+        df = df.combine_first(getattr(data, attr))
+    return df
+
+
+def strip(c):
+    for s in "UCCA_", "CoNLL-U_", "_labeled", "_f1", "_ref":
+        c = c.replace(s, "")
+    return c
 
 
 def main():
     reports = [Report.create(f) for p in sys.argv[1:] or
-               ["out/eval/*/ucca/*.csv", "out/eval/*/ud/*.csv"] for f in glob(p) or [p]]
+               ["eval/*/ucca/*.csv", "eval/*/ud/*.csv"] for f in glob(p) or [p]]
     key = attrgetter("corpus")
     for tup in groupby(sorted(reports, key=key), key=key):
         eval_corpus(*tup)
