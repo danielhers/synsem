7559b857f1e2f5bb6db62b51c462dfcf901b7a47
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-11-08 14:12:14 +0200

    Omit punctuation at comparison time by looking at both passages

--- eval_depcl.py
+++ eval_depcl.py
@@ -1,25 +1,24 @@
 from argparse import ArgumentParser
 from functools import partial
 from operator import attrgetter
+from semstr.conversion.conllu import PUNCT_TAG
 
 from semstr.convert import FROM_FORMAT
-from spacy.parts_of_speech import PUNCT
 from ucca import layer0, layer1
 from ucca.evaluation import SummaryStatistics
 from ucca.ioutil import get_passages
-from ucca.textutil import Attr
 
-FROM_FORMAT["conllu"] = partial(FROM_FORMAT["conllu"], dep=True)
+FROM_FORMAT["conllu"] = partial(FROM_FORMAT["conllu"], dep=True, strip_suffixes=False)
 
 
 def get_terminals(unit, visited=None):
     if visited is None:
         visited = set()
-    outgoing = {e for e in set(unit) - visited if not e.attrib.get("remote")
-                and e.tag not in ("punct", layer1.EdgeTags.Punctuation)}
-    return ([] if unit.tag and (unit.tag not in (layer0.NodeTags.Word, layer0.NodeTags.Punct)
-                                or unit.tok[Attr.POS.value] == PUNCT) else [unit]) + \
-        [t for e in outgoing for t in get_terminals(e.child, visited | outgoing)]
+    outgoing = {e for e in set(unit) - visited if not e.attrib.get("remote")}
+    terminals = [t for e in outgoing for t in get_terminals(e.child, visited | outgoing)]
+    if not unit.tag or unit.tag in (layer0.NodeTags.Word, layer0.NodeTags.Punct):  # UD: all nodes; UCCA: only terminals
+        terminals.append(unit)
+    return terminals
 
 
 def subordinate_clauses(passage):
@@ -29,21 +28,24 @@ def subordinate_clauses(passage):
                 yield from map(get_terminals, filter(layer1.FoundationalNode.is_scene, unit.elaborators))
         else:  # UD
             children = [e.child for e in unit if e.tag in ("acl",)]
-            if children:  # UD and there is a coordination
+            if children:  # UD and there is a subordinate clause
                 yield from map(get_terminals, children)
 
 
 def evaluate(guessed, ref):
     assert guessed.ID == ref.ID, "Inconsistent order of passages: %s != %s" % (guessed.ID, ref.ID)
     guessed_yields, ref_yields = [list(subordinate_clauses(p)) for p in (guessed, ref)]
-    g, r = [set(frozenset(t.position for t in y) for y in yields) for yields in (guessed_yields, ref_yields)]
+    punct_positions = {t.position for yields in (guessed_yields, ref_yields) for y in yields for t in y
+                       if t.tag in (layer0.NodeTags.Punct, PUNCT_TAG)}
+    g, r = [set(frozenset(t.position for t in y) - punct_positions for y in yields)
+            for yields in (guessed_yields, ref_yields)]
     common = g & r
     only_g = g - common
     only_r = r - common
     if g or r:
         for i, yields in enumerate((guessed_yields, ref_yields), start=1):
-            conj = [" ".join(map(str, sorted(y, key=attrgetter("position")))) for y in yields]
-            print(guessed.ID, i, " | ".join(conj))
+            clauses = [" ".join(map(str, sorted(y, key=attrgetter("position")))) for y in yields]
+            print(guessed.ID, i, " | ".join(clauses))
     return SummaryStatistics(len(common), len(only_g), len(only_r))
 
 
