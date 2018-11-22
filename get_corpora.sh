857fa4f155c0fef4fbee5f2f2822e05e73c6526a
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-11-22 14:30:19 +0200

    UD v2.3

--- get_corpora.sh
+++ get_corpora.sh
@@ -5,7 +5,7 @@ cd UCCA_English-Wiki
 python -m scripts.split_corpus -lq -t 4113 -d 514  # Split to train, dev and test
 cd ..
 
-git clone https://github.com/UniversalDependencies/UD_English-EWT
+git clone https://github.com/UniversalDependencies/UD_English-EWT --branch=r2.3
 cd UD_English-EWT
 sed -i '/^# newdoc id = reviews/,$d' en_ewt-ud-*.conllu  # Remove the reviews section
 cd ..
