9902877c070bc68528fb9638301813a4f6245140
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-11-22 14:17:46 +0200

    Separate scripts for evaluating UCCA and UD

---
+++ evaluate_ucca.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#SBATCH --mem=1G
+#SBATCH --time=0-1
+#SBATCH --array=0-13
+
+DIR=$PWD
+. models.sh
+echo SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID
+[[ -n "$SLURM_ARRAY_TASK_ID" ]] && CORPORA=(${CORPORA[$SLURM_ARRAY_TASK_ID]})
+
+for d in ${CORPORA[@]}; do
+  OUT=$DIR/eval/$d
+  UD_REF_YIELDS=$PARSED/conllu/${UDPIPE_MODEL[$d]}/$d.conllu
+  [[ -n "${UD_DATA[$d]}" ]] && UD_REF_YIELDS=${UD_DATA[$d]}
+  mkdir -p $OUT/{,all/}{ucca,ud}
+
+  # Evaluate on UCCA
+  REF=${DATA[$d]}
+  if [[ -z "$REF" ]]; then
+    echo No UCCA data for $d, skipping evaluation
+  else
+    for MODEL in ${UCCA_UD_MODEL[$d]} ${UCCA_MODEL[$d]} ${UDPIPE_MODEL[$d]} ${UD_MODEL[$d]} ud; do  # Evaluate on UCCA data
+      GUESSED=$PARSED/xml/$MODEL/$d
+      if [[ -d $GUESSED ]]; then
+        echo $GUESSED $REF
+        run python -m semstr.evaluate $GUESSED $REF --unlabeled -s $OUT/ucca/$MODEL.unlabeled.csv -q $*
+        run python -m semstr.evaluate $GUESSED $REF --constructions=categories -s $OUT/ucca/$MODEL.csv -c $OUT/ucca/$MODEL.counts.csv -o $OUT/all/ucca/$MODEL.csv -q $*
+        run python -m semstr.evaluate $GUESSED $REF --constructions=categories -s $OUT/ud/$MODEL.csv -c $OUT/ud/$MODEL.counts.csv -o $OUT/all/ud/$MODEL.csv --ref-yield-tags=$UD_REF_YIELDS -q $*
+      else
+        echo $GUESSED not found, cannot evaluate $MODEL
+      fi
+    done
+  fi
+done
+
--- evaluate_ud.sh
+++ evaluate_ud.sh
@@ -10,7 +10,6 @@ echo SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID
 
 for d in ${CORPORA[@]}; do
   OUT=$DIR/eval/$d
-  UD_REF_YIELDS=$PARSED/conllu/${UDPIPE_MODEL[$d]}/$d.conllu
   mkdir -p $OUT/{,all/}{ucca,ud}
 
   # Evaluate on UD
@@ -18,7 +17,6 @@ for d in ${CORPORA[@]}; do
     echo No UD data for $d, skipping evaluation
   else
     REF=${UD_DATA[$d]}
-    UD_REF_YIELDS=$REF
     for MODEL in ${UCCA_UD_MODEL[$d]} ${UCCA_MODEL[$d]} ${UDPIPE_MODEL[$d]} ${UD_MODEL[$d]} ucca; do  # Evaluate w.r.t. gold UD
       GUESSED=$PARSED/conllu/$MODEL/$d
       [[ -d $GUESSED ]] || GUESSED=$GUESSED.conllu
@@ -29,24 +27,6 @@ for d in ${CORPORA[@]}; do
           run python -m semstr.evaluate --no-enhanced $GUESSED $REF --constructions=categories -s $OUT/ucca/$MODEL.ud.csv -c $OUT/ucca/$MODEL.ud.counts.csv -o $OUT/all/ucca/$MODEL.ud.csv -q --ref-yield-tags=${DATA[$d]} $*
         fi
         run python -m semstr.evaluate --no-enhanced $GUESSED $REF --constructions=categories -s $OUT/ud/$MODEL.ud.csv -c $OUT/ud/$MODEL.ud.counts.csv -o $OUT/all/ud/$MODEL.ud.csv -q $*
-      else
-        echo $GUESSED not found, cannot evaluate $MODEL
-      fi
-    done
-  fi
-
-  # Evaluate on UCCA
-  REF=${DATA[$d]}
-  if [[ -z "$REF" ]]; then
-    echo No UCCA data for $d, skipping evaluation
-  else
-    for MODEL in ${UCCA_UD_MODEL[$d]} ${UCCA_MODEL[$d]} ${UDPIPE_MODEL[$d]} ${UD_MODEL[$d]} ud; do  # Evaluate on UCCA data
-      GUESSED=$PARSED/xml/$MODEL/$d
-      if [[ -d $GUESSED ]]; then
-        echo $GUESSED $REF
-        run python -m semstr.evaluate $GUESSED $REF --unlabeled -s $OUT/ucca/$MODEL.unlabeled.csv -q $*
-        run python -m semstr.evaluate $GUESSED $REF --constructions=categories -s $OUT/ucca/$MODEL.csv -c $OUT/ucca/$MODEL.counts.csv -o $OUT/all/ucca/$MODEL.csv -q $*
-        run python -m semstr.evaluate $GUESSED $REF --constructions=categories -s $OUT/ud/$MODEL.csv -c $OUT/ud/$MODEL.counts.csv -o $OUT/all/ud/$MODEL.csv --ref-yield-tags=$UD_REF_YIELDS -q $*
       else
         echo $GUESSED not found, cannot evaluate $MODEL
       fi
