8c53de271be08f45c5b6dd4c2fe41f2ad8d807ab
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-11-23 19:04:21 +0200

    Don't count elided predicate nodes

--- count_deprel.sh
+++ count_deprel.sh
@@ -8,6 +8,6 @@ for d in ${CORPORA[@]}; do
   echo $d
   CONLLU_FILE=$PARSED/conllu/${UDPIPE_MODEL[$d]}/$d.conllu
   [[ -n "${UD_DATA[$d]}" ]] && CONLLU_FILE=${UD_DATA[$d]}
-  awk '{if(/^[0-9]/){sub(/:.*|_/,"",$8);if($8)print $8}}' $CONLLU_FILE | sort | uniq -c | awk '{print $2,$1}' | jq -R . | jq -sr 'map(./" ") | transpose | map(join("\t"))[]'
+  awk '{if(/^[0-9]*\t/){sub(/:.*|_/,"",$8);if($8)print $8}}' $CONLLU_FILE | sort | uniq -c | awk '{print $2,$1}' | jq -R . | jq -sr 'map(./" ") | transpose | map(join("\t"))[]'
 done
 
