5e0d496a16f495ee2935639d7cd9c1e133995de5
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-12-05 09:37:31 +0200

    Count only relations included in evaluation

--- count_deprel.sh
+++ count_deprel.sh
@@ -8,6 +8,7 @@ for d in ${CORPORA[@]}; do
   echo $d
   CONLLU_FILE=$PARSED/conllu/${UDPIPE_MODEL[$d]}/$d.conllu
   [[ -n "${UD_DATA[$d]}" ]] && CONLLU_FILE=${UD_DATA[$d]}
-  awk '{if(/^[0-9]*\t/){sub(/:.*|_/,"",$8);if($8)print $8}}' $CONLLU_FILE | sort | uniq -c | awk '{print $2,$1}' | jq -R . | jq -sr 'map(./" ") | transpose | map(join("\t"))[]'
+  echo $CONLLU_FILE
+  awk '{if(/^[0-9]*\t/){sub(/:.*|_/,"",$8);if($8)print $8}}' $CONLLU_FILE | sort | uniq -c | grep -wf included_rels.txt | awk '{print $2,$1}' | jq -R . | jq -sr 'map(./" ") | transpose | map(join("\t"))[]'
 done
 
