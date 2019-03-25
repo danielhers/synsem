68653bdf10702de6f23358dfd828cc9be5e49d30
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-03-25 14:19:31 +0100

    Script to calculate the confusion matrix in the paper

---
+++ confusion_matrix.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+pip install -U semstr udapi
+mkdir -p data/ucca-train-dev
+cd data
+[[ -d sentences ]] || git clone https://github.com/UniversalConceptualCognitiveAnnotation/UCCA_English-EWT --branch=v1.0-sentences sentences
+[[ -d ud ]] || git clone https://github.com/UniversalConceptualCognitiveAnnotation/UCCA_English-EWT --branch=v1.0-ud ud
+[[ -d UD_English-EWT ]] || git clone https://github.com/UniversalDependencies/UD_English-EWT --branch=r2.3
+[[ -f split.sh ]] || wget https://raw.githubusercontent.com/UniversalConceptualCognitiveAnnotation/UCCA_English-EWT/master/scripts/split.sh
+chmod +x split.sh
+./split.sh
+cp -L sentences/{train,dev}/* ucca-train-dev
+python -m semstr.scripts.annotate ud/train -o ud-train-dev -c ud/train -a -l en || exit 1
+python -m semstr.scripts.annotate ud/dev -o ud-train-dev -c ud/dev -a -l en || exit 1
+python -m scripts.evaluate_standard ud-train-dev ucca-train-dev --errors --as-table
